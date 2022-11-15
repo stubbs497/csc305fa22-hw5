8469eadfbb8ba20dd96c81f800012e68a99db657
stubbs497 <c.jameson.s1002@gmail.com>
2022-11-15 12:49:33 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', formdata: undefined });
+});
+
+router.post('/', function(req, res, next) {
+  res.render('index', { title: 'Express', formdata: req.body });
 });
 
 module.exports = router;
